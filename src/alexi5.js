5f0b9102de0bb290366d202e2543c3e3c3c4ea26
VEEGISHx <veegish240@gmail.com>
2019-03-30 23:27:25 +0400

    Updated .gitignore

--- src/alexi5.js
+++ src/alexi5.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 var servers = {};
- 
+
 const got = require('got');
 const ytdl = require('ytdl-core');
 const config = require('./config.json');
@@ -22,13 +22,13 @@ var timestamp = '[' + time.getHours() + ':' + time.getMinutes() + ':' + time.get
 function stream(connection, msg) {
     var server = servers[msg.guild.id];
     // audioonly || highestaudio
-    server.dispatcher = connection.playStream(ytdl(server.queue[0], {quality: "highestaudio"}));
+    server.dispatcher = connection.playStream(ytdl(server.queue[0], { quality: "highestaudio" }));
     console.log('stream:' + server.queue);
 
     server.dispatcher.on("end", function() {
         // Remove the current song from the queue
         server.queue.shift();
-        if(server.queue[0]) {
+        if (server.queue[0]) {
             // Keep streaming as long as there is at least 1 item in queue
             stream(connection, msg);
         } else {
@@ -41,7 +41,7 @@ function stream(connection, msg) {
 bot.on('ready', () => {
     console.log(`${timestamp} Logged in as ${bot.user.tag}!`);
     console.log(`--------------------------------------------`);
-    console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
+    console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
     bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
     console.log(`Bot is up and running`);
     console.log(`--------------------------------------------`);
@@ -53,7 +53,7 @@ bot.on("guildCreate", guild => {
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
     bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
 });
-  
+
 bot.on("guildDelete", guild => {
     // this event triggers when the bot is removed from a guild.
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
@@ -61,46 +61,46 @@ bot.on("guildDelete", guild => {
 });
 
 bot.on('message', async msg => {
-    if(msg.content.substring(0, 2) == prefix) {
+    if (msg.content.substring(0, 2) == prefix) {
         var sentence = msg.content.slice(2);
         var args = sentence.split(" ");
 
         // It's good practice to ignore other bots. This also makes your bot ignore itself
         // and not get into a spam loop (we call that "botception").
-        if(msg.author.bot) return;
-    
+        if (msg.author.bot) return;
+
         // Also good practice to ignore any message that does not start with our prefix, 
         // which is set in the configuration file.
-        if(msg.content.indexOf(config.prefix) !== 0) return;
+        if (msg.content.indexOf(config.prefix) !== 0) return;
 
-        if(args[0] == undefined || !args[0].trim().length) {
+        if (args[0] == undefined || !args[0].trim().length) {
             msg.channel.send('Error: You need to specify at least 1 argument');
             msg.channel.send(`Try **${prefix}help** for a list of options`);
         }
 
-        switch(args[0].toLowerCase()) {
-            case 'help' :
+        switch (args[0].toLowerCase()) {
+            case 'help':
                 listCommands(msg);
                 break;
-            case 'meme' :
+            case 'meme':
                 meme(msg);
                 break;
-            case 'joke' :
+            case 'joke':
                 joke(msg);
                 break;
             case 'amazeme':
                 amazeme(msg);
                 break;
-            case 'list' :
+            case 'list':
                 listCommands(msg);
                 break;
-            case 'ping' :
+            case 'ping':
                 const m = await msg.channel.send("Ping?");
                 m.edit(`Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
                 break;
-            case 'music' :
-            // music {args[1]} || music {args[1]} {args[2]}
-                if(!args[1]) {
+            case 'music':
+                // music {args[1]} || music {args[1]} {args[2]}
+                if (!args[1]) {
                     const embed = new Discord.RichEmbed();
                     embed.setTitle(':musical_note:  Music Streaming  :musical_note: ')
                     embed.setDescription('Stream music from YouTube using the commands below');
@@ -118,7 +118,7 @@ bot.on('message', async msg => {
                         return;
                     }
 
-                    if(!servers[msg.guild.id]) {
+                    if (!servers[msg.guild.id]) {
                         servers[msg.guild.id] = {
                             queue: []
                         }
@@ -126,8 +126,8 @@ bot.on('message', async msg => {
 
                     var server = servers[msg.guild.id];
 
-                    if(args[1] == 'play') {
-                        if(!msg.guild.voiceChannel) {
+                    if (args[1] == 'play') {
+                        if (!msg.guild.voiceChannel) {
                             msg.member.voiceChannel.join().then(connection => {
                                 // Append song to queue
                                 stream(connection, msg);
@@ -141,14 +141,14 @@ bot.on('message', async msg => {
                     console.log(args[1]);
                     console.log(args[2]);
                     console.log('========================================');
-    
 
-    
-                    if(args[1] == 'add') {
+
+
+                    if (args[1] == 'add') {
                         var server = servers[msg.guild.id];
                         // Playlist resolver
                         var playlistResolve = 'playlist';
-                        if(args[2].includes(playlistResolve)) {
+                        if (args[2].includes(playlistResolve)) {
                             var playlistId = args[2].split('=').pop();
                             console.log('id: ' + playlistId);
                             got(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${ytkey}&maxResults=50`).then(response => {
@@ -159,57 +159,58 @@ bot.on('message', async msg => {
                                 // The list API doesn't provide a property for videoIDs
                                 // Instead the video IDs are provided in the thumbnail URLs
                                 // Building the video URLS after extracting the IDs from the thumbnails
-                                for(var video in result.items) {
-                                   if(!result.items[video].snippet.title.includes(flag)) {
-                                       var videoId = result.items[video].snippet.thumbnails.default.url.replace('https://i.ytimg.com/vi/', '').replace('/default.jpg', '');  
-                                       var videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
-                                       server.queue.push(videoUrl); 
-                                       titleQueue.push(result.items[video].snippet.title);
-                                       console.log(titleQueue);
-                                   }
+                                for (var video in result.items) {
+                                    if (!result.items[video].snippet.title.includes(flag)) {
+                                        var videoId = result.items[video].snippet.thumbnails.default.url.replace('https://i.ytimg.com/vi/', '').replace('/default.jpg', '');
+                                        var videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+                                        server.queue.push(videoUrl);
+                                        titleQueue.push(result.items[video].snippet.title);
+                                        console.log(titleQueue);
+                                    }
                                 }
                                 msg.channel.send(`Playlist successfully parsed and loaded. ${count} songs have been added to the queue.`)
-                                .then(sent => console.log(`A playlist has been loaded`));
-                                if(count > 200) {
+                                    .then(sent => console.log(`A playlist has been loaded`));
+                                if (count > 200) {
                                     msg.channel.send(`Wait what?? HOLYSHIT ${count} songs...`)
                                 }
 
                             })
                         } else {
                             server.queue.push(args[2]);
-                            msg.delete().catch(O_o=>{});
+                            msg.delete().catch(O_o => {});
                             msg.channel.send("Song added. Gotta improve this response someday...maybe add a timeout :thinking:");
                             console.log('add:' + server.queue);
                         }
                     }
 
-                    if(args[1] == 'skip') {
+                    if (args[1] == 'skip') {
                         var server = servers[msg.guild.id];
-                        if(server.dispatcher) {
+                        if (server.dispatcher) {
                             server.dispatcher.end();
                         }
-                    }               
+                    }
                 }
                 break;
-            case 'leave' :
-                if(msg.guild.voiceConnection) {
+            case 'leave':
+                if (msg.guild.voiceConnection) {
                     msg.guild.voiceConnection.disconnect();
                     msg.channel.send("Stream has ended ʕ•ᴥ•ʔ");
                 }
                 break;
-            case 'purge' :
+            case 'purge':
                 const deleteCount = parseInt(args[1], 10);
-                
-                if(!deleteCount || deleteCount < 1 || deleteCount > 100)
-                return msg.reply("Please provide a number between 1 and 100 for the number of messages to delete");
-                
-                const fetched = await msg.channel.fetchMessages({limit: deleteCount});
+
+                if (!deleteCount || deleteCount < 1 || deleteCount > 100)
+                    return msg.reply("Please provide a number between 1 and 100 for the number of messages to delete");
+
+                const fetched = await msg.channel.fetchMessages({ limit: deleteCount });
                 msg.channel.bulkDelete(fetched).catch(error => msg.reply(`Couldn't delete messages because of: ${error}`));
                 msg.channel.send(`PURGE! PURGE! PURGE! ${deleteCount} messages have been slaughtered :)`)
-            break; 
-            default: msg.channel.send('This feature hasn\'t been implemented yet :slight_frown:');
+                break;
+            default:
+                msg.channel.send('This feature hasn\'t been implemented yet :slight_frown:');
         }
     }
 });
-  
-  bot.login(token);
+
+bot.login(token);
