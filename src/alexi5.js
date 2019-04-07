6ef50d1eab60f92eb81304324bc4b9db9e73365c
veegish <veegish240@gmail.com>
2019-04-07 12:23:50 +0400

    Added ask functionality

--- src/alexi5.js
+++ src/alexi5.js
@@ -101,6 +101,27 @@ bot.on('message', async msg => {
             case 'amazeme':
                 amazeme(msg);
                 break;
+            case 'ask':
+                if (!args[1]) {
+                    msg.reply("Please ask a question `+!ask your_question`");
+                } else {
+                    let replies = [
+                        'Yes.',
+                        'Yes.',
+                        'No.',
+                        'Perhaps you should consider it.',
+                        'Probably.',
+                        'Indeed',
+                        'Just get over with it.',
+                        'Maybe, maybe not. Maybe go fuck yourself.',
+                        'When the sun rises north, and when the pigs fly, then thou shalt find what you seek.'
+                    ];
+
+                    let botreply = replies[Math.floor(Math.random() * replies.length)];
+                    msg.reply(botreply);
+                }
+
+                break;
             case 'meeting':
                 if (!args[1]) {
                     msg.channel.send("Please specify a time for the meeting.");
