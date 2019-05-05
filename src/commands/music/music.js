562c535edbe31485b3db0d53d32bfa367e48d239
veegish <veegish240@gmail.com>
2019-05-05 21:10:18 +0400

    Refactored code for response on track skip

--- src/commands/music/music.js
+++ src/commands/music/music.js
@@ -116,7 +116,6 @@ module.exports = function music(ytkey, servers, titles, msg, prefix, musicComman
             if (server.dispatcher) {
                 title.queue.shift();
                 server.dispatcher.end();
-                msg.channel.send(`Now playing **${title.queue[1]}**`);
             }
         }
 
