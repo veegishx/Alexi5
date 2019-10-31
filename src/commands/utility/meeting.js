d1a000c830f9b055f2a5f4b176881b59588bcfa0
Veegish Ramdani <veegish@cyberstorm.mu>
2019-10-31 21:40:47 +0400

    Removed unfinished feature

--- src/commands/utility/meeting.js
+++
@@ -1,8 +0,0 @@
-//const prefix = config.prefix;
-const Discord = require('discord.js');
-const moment = require('moment-timezone');
-let i = 0;
-module.exports = function info(msg, time, title, option) {
-    let value = time * 60;
-    msg.channel.send(`@everyone Meeting **${title}** is due in :clock2: ${time} minutes`);
-}
